49790d39a076de2212ed5a53c3bd74dc32094b53
cyclotruc <romain@coderamp.io>
2024-12-07 18:49:15 +0000

    Add copy whole digest button

--- src/static/js/utils.js
+++ src/static/js/utils.js
@@ -87,6 +87,29 @@ function handleSubmit(event, showLoading = false) {
         });
 }
 
+function copyFullDigest() {
+    const directoryStructure = document.querySelector('.directory-structure').value;
+    const filesContent = document.querySelector('.result-text').value;
+    const fullDigest = `Directory Structure:\n\n${directoryStructure}\n\nFiles Content:\n\n${filesContent}`;
+    const button = document.querySelector('[onclick="copyFullDigest()"]');
+    const originalText = button.innerHTML;
+
+    navigator.clipboard.writeText(fullDigest).then(() => {
+        button.innerHTML = `
+            <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+            </svg>
+            Copied!
+        `;
+
+        setTimeout(() => {
+            button.innerHTML = originalText;
+        }, 2000);
+    }).catch(err => {
+        console.error('Failed to copy text: ', err);
+    });
+}
+
 // Export functions if using modules
 window.copyText = copyText;
 window.handleSubmit = handleSubmit; 

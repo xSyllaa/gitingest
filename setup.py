eb814b4180101e50840e5ad790e9be6f68fb0fdc
cyclotruc <romain@coderamp.io>
2024-12-22 06:34:45 +0000

    Edit setup

--- setup.py
+++ setup.py
@@ -2,20 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="gitingest",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
         "click>=8.0.0",
-        "tokencost",
+        "tiktoken",
     ],
-    extras_require={
-        'dev': [
-            'pytest',
-            'pytest-asyncio',
-        ],
-    },
     entry_points={
         "console_scripts": [
             "gitingest=gitingest.cli:main",
@@ -33,9 +27,5 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
     ],
 )

316bda71d4cfbcd37207fc42803091afcc06d6b6
xSylla <cryptosylla@gmail.com>
2025-02-18 11:51:02 +0100

    fork

--- src/gitingest/__init__.py
+++
@@ -1,8 +0,0 @@
-""" Gitingest: A package for ingesting data from Git repositories. """
-
-from gitingest.query_ingestion import run_ingest_query
-from gitingest.query_parser import parse_query
-from gitingest.repository_clone import clone_repo
-from gitingest.repository_ingest import ingest, ingest_async
-
-__all__ = ["run_ingest_query", "clone_repo", "parse_query", "ingest", "ingest_async"]
